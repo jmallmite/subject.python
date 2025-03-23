173007abfded2f96ac9f9b6358af8d6f2f7df6df
jmallmite <jmallmite@gmail.com>
2025-03-23 22:20:29 +0900

    Create computer.py

---
+++ computer.py
@@ -0,0 +1,20 @@
+class Computer:
+    def __init__(self, cpu, memory, disk):
+        self.cpu = cpu
+        self.memory = memory
+        self.disk = disk
+
+    def get_cpu(self):
+        return self.cpu
+
+    def get_memory(self):
+        return self.memory
+
+    def get_disk(self):
+        return self.disk
+
+# 사용 예시
+my_computer = Computer("Intel Core i7", 16, 512)
+print(my_computer.get_cpu())
+print(my_computer.get_memory())
+print(my_computer.get_disk())
